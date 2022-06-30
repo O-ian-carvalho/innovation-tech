1c580f2be0e75a652e364898e76c2b0989deea5c
O-ian-carvalho <108496720+O-ian-carvalho@users.noreply.github.com>
2022-06-30 19:49:15 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+const zap = document.querySelector('.whatsapp')
+const email = document.querySelector('.email')
+const telefone = document.querySelector('.telefone')
+
+
+
+
+
+telefone.addEventListener('click', ()=>{
+    alert('Ligue para (75) 9 8147-5385')
+})
+
+email.addEventListener('click', ()=>{
+    alert('entre em contato pelo email matheus.dias77@aluno.enova.educacao.ba.gov.br')
+})
+
+zap.addEventListener('click', ()=>{
+    alert('entre em contato pelo whatsApp (75) 9 8147-5385')
+})
